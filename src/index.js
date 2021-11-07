7ee4c1e4a0ebe82280be3415a3c5e8aed868d454
hazartilirot <hazartilirot@gmail.com>
2021-11-07 02:19:22 +0200

    Switching to Workbox since service-worker is no longer created in a usual create-react-app as building the project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers';
 import App from './containers/App.jsx'
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'tachyons';
 
@@ -19,4 +20,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
